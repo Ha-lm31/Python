5877276ee0a873155fc85bc8fceecab4ed16d353
Aml Halouma <amal.halouma31@gmail.com>
2026-09-08 15:37:05 +0000

    08-09-2026

---
+++ 1.Lessons/1.TUTORIAL/11.StringMethods.py
@@ -0,0 +1,93 @@
+# d-1 : 08-09-2026
+print("1.capitalize() : Converts the first character to upper case")
+print("Python String capitalize() Method")
+txt = "hello, and welcome to my world."
+x = txt.capitalize()
+print(f'''txt:                  {txt}
+x = txt.capitalize(): {x}''')
+txt = "python is FUN!"
+x = txt.capitalize()
+print(f'''txt:                  {txt}
+x = txt.capitalize(): {x}''')
+txt = "36 is my age."
+x = txt.capitalize()
+print(f'''txt:                  {txt}
+x = txt.capitalize(): {x}''')
+
+print("\n2.casefold() : Converts string into lower case")
+print("Python String casefold() Method")
+txt = "Hello, And Welcome To My World!"
+x = txt.casefold()
+print(f'''txt:                {txt}
+x = txt.casefold(): {x}''')
+
+print("\n3.center() : Returns a centered string")
+print("Python String center() Method")
+txt = "banana"
+x = txt.center(20)
+print(f'''txt:                {txt}
+x = txt.center(20): {x}''')
+print("Syntax : 'string.center(length, character)'")
+txt = "banana"
+x = txt.center(20, "O")
+print(f'''txt:                     {txt}
+x = txt.center(20, "O"): {x}''')
+###########################################################################################################
+print("4.count() : ")
+
+print("5.encode() : ")
+
+print("6.endswith() : ")
+'''
+print("7.")
+print("8.")
+print("9.")
+
+print("10.")
+print("11.")
+print("12.")
+
+print("13.")
+print("14.")
+print("15.")
+
+print("16.")
+print("17.")
+print("18.")
+
+print("19.")
+print("20.")
+print("21.")
+
+print("22.")
+print("23.")
+print("24.")
+
+print("25.")
+print("26.")
+print("27.")
+
+print("28.")
+print("29.")
+print("30.")
+
+print("31.")
+print("32.")
+print("33.")
+
+print("34.")
+print("35.")
+print("36.")
+
+print("37.")
+print("38.")
+print("39.")
+
+print("40.")
+print("41.")
+print("42.")
+
+print("43.")
+print("44.")
+print("45.")
+'''
--- 1.Lessons/1.TUTORIAL/12.Booleans.py
+++ 1.Lessons/1.TUTORIAL/12.Booleans.py
@@ -1,1 +1,52 @@
-print("---")
+print("1---Python Booleans")
+print("Booleans represent one of two values: True or False.")
+print("1.Boolean Values")
+print("10 > 9: ",10 > 9)
+print("10 == 9: ",10 == 9)
+print("10 < 9: ",10 < 9)
+a = 200
+b = 33
+print(f"a: {a}, b: {b}")
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+print("2.Evaluate Values and Variables")
+print('bool("Hello") :',bool("Hello"))
+print("bool(15): ",bool(15))
+x = "Hello"
+y = 15
+print(f"x : {x}, y : {y}")
+print("bool(x): ",bool(x))
+print("bool(y): ",bool(y))
+print("3.Most Values are True & 4.Some Values are False")
+print('bool("abc"): ',bool("abc"))
+print("bool(123): ",bool(123))
+print('bool(["apple", "cherry", "banana"]): ',bool(["apple", "cherry", "banana"]))
+print("bool(False): ",bool(False))
+print("bool(None): ",bool(None))
+print("bool(0): ",bool(0))
+print('bool(""): ',bool(""))
+print("bool(()): ",bool(()))
+print("bool([]): ",bool([]))
+print("bool({}): ",bool({}))
+class myclass():
+  def __len__(self):
+    return 0
+myobj = myclass()
+print("bool(myobj): ",bool(myobj))
+print("5.Functions can Return a Boolean")
+def myFunction() :
+  return True
+print(myFunction())
+def myFunction() :
+  return True
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+x = 200
+print("x: ",x)
+print("isinstance(x, int): ",isinstance(x, int))
+
+print("\n2---Python Booleans Code Challenge")
